1a71b1d80d124220c7183ae3ecbce747912ff090
slarribeau <slarribeau@gmail.com>
2019-02-11 22:28:35 -0800

    got a second question limping

--- src/app/questions/q1.gt.year.lt.25.component.ts
+++ src/app/questions/q1.gt.year.lt.25.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item';
+
+
+
+export const ITEMS: Item[] = [
+  {
+      name:'Are you a BORG?',
+      value:'q0'
+  },
+  {
+      name:'Dominoes?',
+      value:'q1'
+  },
+  {
+      name:'Chinese Food!',
+      value:'q2'
+  },
+];
 
 @Component({
   selector: 'app-q1GTyearLT25',
@@ -6,12 +24,23 @@ import { Component, OnInit } from '@angular/core';
     <p>
       Q1 (greater than year && less than 25)!
     </p>
+    <div>
+    <ul class="list-group">
+      <li class="list-group-item" *ngFor="let item of itemsList">
+        <input type="radio" [(ngModel)]="radioSelected" name="list_name" value="{{item.value}}" (change)="onItemChange(item)" />
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
   `,
   styles: []
 })
-export class q1GTyearLT25Component implements OnInit {
 
-  constructor() { }
+export class q1GTyearLT25Component implements OnInit {
+  itemsList: Item[] = ITEMS;
+  constructor() {
+    this.itemsList = ITEMS;
+   }
 
   ngOnInit() {
   }
