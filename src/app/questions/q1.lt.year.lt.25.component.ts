d2c9579a55f165512ee3f63510e39e85eca5ad4c
slarribeau <slarribeau@gmail.com>
2019-02-12 21:30:41 -0800

    support 2nd question

--- src/app/questions/q1.lt.year.lt.25.component.ts
+++ src/app/questions/q1.lt.year.lt.25.component.ts
@@ -1,19 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Item } from './item';
+import {C2p2InputService} from '../services/c2p2-input.service'
+
+
+
+
+export const ITEMS: Item[] = [
+  {
+      name:'Are you a boy?',
+      value:'q0'
+  },
+  {
+      name:'chili?',
+      value:'q1'
+  },
+  {
+      name:'russian Food!',
+      value:'q2'
+  },
+];
 @Component({
   selector: 'app-q1LTyearLT25',
   template: `
     <p>
       Q1 (less than year && less than 25)!
     </p>
+    <div>
+    <ul class="list-group">
+      <li class="list-group-item" *ngFor="let item of itemsList">
+        <input type="radio" [(ngModel)]="radioSelected" name="list_name" value="{{item.value}}" (change)="onItemChange(item)" />
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
   `,
   styles: []
 })
 export class q1LTyearLT25Component implements OnInit {
-
-  constructor() { }
+  itemsList: Item[] = ITEMS;
+  radioSel: any;
+  radioSelected: string;
+  radioSelectedString: string;
+  constructor(private myInputService:C2p2InputService) {
+    this.itemsList = ITEMS;
+    //Selecting Default Radio item here
+    this.radioSelected = "item_3";
+  }
 
   ngOnInit() {
   }
-
+  // Radio Change Event
+  onItemChange(item) {
+    console.log(item)
+    //console.log("about to emit from main component to parent")
+   // this.childEvent.emit("scott");
+    this.myInputService.notifyParent(this.radioSelected);
+  }
 }
--- src/app/questions/q2.gt.year.lt.25.component.ts
+++ src/app/questions/q2.gt.year.lt.25.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item';
+import {C2p2InputService} from '../services/c2p2-input.service'
+
+export const ITEMS: Item[] = [
+  {
+      name:'Are you a golfer?',
+      value:'q0'
+  },
+  {
+      name:'tennis?',
+      value:'q1'
+  },
+  {
+      name:'dog Food!',
+      value:'q2'
+  },
+];
 
 @Component({
   selector: 'app-q2GTyearLT25',
@@ -6,14 +23,36 @@ import { Component, OnInit } from '@angular/core';
     <p>
       Q1 (Greater than year && less than 25)!
     </p>
+    <div>
+    <ul class="list-group">
+      <li class="list-group-item" *ngFor="let item of itemsList">
+        <input type="radio" [(ngModel)]="radioSelected" name="list_name" value="{{item.value}}" (change)="onItemChange(item)" />
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
   `,
   styles: []
 })
-export class q2GTyearLT25Component implements OnInit {
 
-  constructor() { }
+export class q2GTyearLT25Component implements OnInit {
+  itemsList: Item[] = ITEMS;
+  radioSel: any;
+  radioSelected: string;
+  radioSelectedString: string;
+  constructor(private myInputService:C2p2InputService) {
+    this.itemsList = ITEMS;
+    //Selecting Default Radio item here
+    this.radioSelected = "item_3";
+  }
 
   ngOnInit() {
   }
-
+  // Radio Change Event
+  onItemChange(item) {
+    console.log(item)
+    //console.log("about to emit from main component to parent")
+   // this.childEvent.emit("scott");
+    this.myInputService.notifyParent(this.radioSelected);
+  }
 }
