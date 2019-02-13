e95e7a3058239aa06eabe4a40e821b9a61f99abd
slarribeau <slarribeau@gmail.com>
2019-02-13 13:41:58 -0800

    refactor

--- src/app/questions/q1.component.ts
+++ src/app/questions/q1.component.ts
@@ -2,24 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Item } from './item';
 import {Q1Service} from '../services/q1.service'
 
-
-
-
-export const ITEMS: Item[] = [
-  {
-      name:'Are you a BORG?',
-      value:'q0'
-  },
-  {
-      name:'Dominoes?',
-      value:'q1'
-  },
-  {
-      name:'Chinese Food!',
-      value:'q2'
-  },
-];
-
 @Component({
   selector: 'app-q1',
   template: `
@@ -28,7 +10,7 @@ export const ITEMS: Item[] = [
     </p>
     <div>
     <ul class="list-group">
-      <li class="list-group-item" *ngFor="let item of itemsList2">
+      <li class="list-group-item" *ngFor="let item of itemsList">
         <input type="radio" [(ngModel)]="questionSelected" name="list_name" value="{{item.value}}" (change)="onItemChange(item)" />
         {{item.name}}
       </li>
@@ -39,19 +21,16 @@ export const ITEMS: Item[] = [
 })
 
 export class Q1Component implements OnInit {
-  itemsList: Item[] = ITEMS;
-  //itemsList2: Item[] =[]
   questionSelected: string;
-  @Input() itemsList2: Item[]; 
+  @Input() itemsList: Item[]; 
 
-  constructor(private myInputService:Q1Service) {
-    this.itemsList = ITEMS;
-  }
+  constructor(private myInputService:Q1Service) {}
 
   ngOnInit() {
     console.log("constructor");
-    console.log(this.itemsList2)
+    console.log(this.itemsList)
   }
+  
   onItemChange(item) {
     console.log(item)
     this.myInputService.notifyParent(this.questionSelected);
