4ab55a057533226a61d3beebca3ac3efaca40e6a
slarribeau <slarribeau@gmail.com>
2019-02-13 14:09:44 -0800

    clean up q2

--- src/app/questions/q2.gt.year.lt.25.component.ts
+++ src/app/questions/q2.gt.year.lt.25.component.ts
@@ -1,58 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './item';
-import {Q1Service} from '../services/q1.service'
-
-export const ITEMS: Item[] = [
-  {
-      name:'Are you a golfer?',
-      value:'q0'
-  },
-  {
-      name:'tennis?',
-      value:'q1'
-  },
-  {
-      name:'dog Food!',
-      value:'q2'
-  },
-];
+import {Q2Service} from '../services/q2.service'
+import {Q2Component} from './q2.component'
 
 @Component({
   selector: 'app-q2GTyearLT25',
   template: `
-    <p>
-      Q1 (Greater than year && less than 25)!
-    </p>
-    <div>
-    <ul class="list-group">
-      <li class="list-group-item" *ngFor="let item of itemsList">
-        <input type="radio" [(ngModel)]="radioSelected" name="list_name" value="{{item.value}}" (change)="onItemChange(item)" />
-        {{item.name}}
-      </li>
-    </ul>
-  </div>
+  <app-q1 [itemsList]=ITEMS></app-q1>
   `,
   styles: []
 })
 
 export class q2GTyearLT25Component implements OnInit {
-  itemsList: Item[] = ITEMS;
-  radioSel: any;
-  radioSelected: string;
-  radioSelectedString: string;
-  constructor(private myInputService:Q1Service) {
-    this.itemsList = ITEMS;
-    //Selecting Default Radio item here
-    this.radioSelected = "item_3";
+  ITEMS: Item[]
+  constructor(){
+     this.ITEMS = [
+      {
+          name:'1111Are you a BORG?',
+          value:'R0'
+      },
+      {
+          name:'1111Dominoes?',
+          value:'R1'
+      },
+      {
+          name:'1111Chinese Food!',
+          value:'R2'
+      },
+    ];
   }
-
   ngOnInit() {
-  }
-  // Radio Change Event
-  onItemChange(item) {
-    console.log(item)
-    //console.log("about to emit from main component to parent")
-   // this.childEvent.emit("scott");
-    this.myInputService.notifyParent(this.radioSelected);
+    console.log(this.ITEMS)
   }
 }
--- src/app/questions/q2.lt.year.lt.25.component.ts
+++ src/app/questions/q2.lt.year.lt.25.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item';
+import {Q2Service} from '../services/q2.service'
+import {Q2Component} from './q2.component'
 
 @Component({
   selector: 'app-q2LTyearLT25',
   template: `
-    <p>
-      Q2 (less than year && less than 25)!
-    </p>
+    <app-q1 [itemsList]=ITEMS></app-q1>
   `,
   styles: []
 })
+
 export class q2LTyearLT25Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ITEMS: Item[]
+  constructor(){
+     this.ITEMS = [
+      {
+          name:'2222Are you a BORG?',
+          value:'R0'
+      },
+      {
+          name:'2222Dominoes?',
+          value:'R1'
+      },
+      {
+          name:'2222Chinese Food!',
+          value:'R2'
+      },
+    ];
   }
-
+  ngOnInit() {
+    console.log(this.ITEMS)
+  }
 }
