e145beeab71a350f91eafb3379e57479fe6bb865
slarribeau <slarribeau@gmail.com>
2019-03-03 11:50:38 -0800

    refactor

--- src/app/answer.component.ts
+++ src/app/answer.component.ts
@@ -13,18 +13,6 @@ export class AnswerComponent implements OnInit {
   @Input() answerOne: string; 
   @Input() answerTwo: string; 
   @Input() answerThree: string; 
-  line1:string;
-  line2:string;
-  line3:string;
-  line4:string;
-  line5:string;
-  line6:string;
-  line7:string;
-  line8:string;
-  line9:string;
-  line10:string;
-  line11:string;
-  line12:string;
   myQuestionSet: QuestionSet;
   score:number;
   foo:QuestionDBService;
@@ -65,8 +53,6 @@ export class AnswerComponent implements OnInit {
       this.answerOne = params.get('answerOne');
       this.answerTwo = params.get('answerTwo');
       this.answerThree = params.get('answerThree');
-      this.line1 = "";
-      this.line2 = this.level+this.answerOne+this.answerTwo+this.answerThree;
     });
     this.myQuestionSet=this.foo.getQuestionSet(this.level);
     this.score=25;
